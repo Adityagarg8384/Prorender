de1b73456a07cc5e2bd97dd7cadc168a29741deb
Adityagarg8384 <adityagarg8384@gmail.com>
2025-01-18 00:07:01 +0530

    Adding data

--- code/newdiseaseprediction/main.py
+++ code/newdiseaseprediction/main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
 import numpy as np
+import os
+
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -75,4 +77,5 @@ def predict_disease():
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(debug=True, host='0.0.0.0', port=port)
